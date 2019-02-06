1ea030dcbed3fa28b5571d5373ad7814b07afc8e
小崎 勇太 <kozakiyuta@LAB-N1494.local>
2019-02-06 10:58:10 +0900

    input Converter was implemented

--- inputconverter.h
+++ inputconverter.h
@@ -2,11 +2,25 @@
 #define INPUTCONVERTER_H
 
 #include <QObject>
+#include "qoscmessage.h"
+#include <QDebug>
 
 class InputConverter
 {
 public:
-    InputConverter();
+    InputConverter(QString);
+    ~InputConverter();
+    int getEleNum();
+    void setMessage();
+    QOSCMessage* getMessage();
+private:
+    QString _row_data;
+    QString _address;
+    int eleNum;
+    QOSCMessage* _message;
+    bool cast_int_result;
+    bool cast_float_result;
+    bool cast_string_result;
 };
 
 #endif // INPUTCONVERTER_H
