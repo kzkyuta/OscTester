a072d51cc9e0878a1afec9294a5d3962f709d3ae
小崎 勇太 <kozakiyuta@LAB-N1494.local>
2019-02-12 17:44:06 +0900

    this version can work

--- osctester.cpp
+++ osctester.cpp
@@ -6,8 +6,8 @@ OscTester::OscTester(QWidget *parent) :
     QMainWindow(parent),
 //    _oscSender(new QOSCSender("127.0.0.1", 10002, this)),
     ui(new Ui::OscTester),
-    _oscReceiver(new QOSCReceiver(10001, this)),
-    _scroll(new VerticalScrollArea(nrows,ncols, this))
+    _oscReceiver(new QOSCReceiver(10001, this))
+//    _scroll(new VerticalScrollArea(nrows,ncols, this))
 {
     connect(_oscReceiver, SIGNAL(messageReceived(QOSCMessage*)), this, SLOT(onMessageReceived(QOSCMessage*)));
 //    connect(ui->actionExit, SIGNAL(triggered()), this, SLOT(close()));
@@ -60,6 +60,5 @@ void OscTester::closeEvent(QCloseEvent *event){
 }
 
 void OscTester::on_addContainer_clicked(){
-    qInfo() << scroll().nRows
 //    int column = scroll.
 }
